aff92723464d47bfb5a924a0cf2c7323bb718318
Anujap04 <116511972+Anujap04@users.noreply.github.com>
2022-10-23 23:04:36 +0530

    Create bounceballzrulez.py

---
+++ bounceballzrulez.py
@@ -0,0 +1,4 @@
+print("The rules for this game are pretty simple")
+print("Only one color of ball can be selected at a time")
+print("Only one use can play game at a time")
+print("Play for fun not for revenge")
