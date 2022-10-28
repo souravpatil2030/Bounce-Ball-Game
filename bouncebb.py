7cbcf1928ce6ddfd4410ad242050179145788a19
harshu20305 <116876642+harshu20305@users.noreply.github.com>
2022-10-28 14:42:09 +0530

    Create bouncebb.py

---
+++ bouncebb.py
@@ -0,0 +1,4 @@
+print("The rules to use and handle this app are simpler")
+print("Once color selected cannot be changed at next step")
+print("Only one user is served at one moment")
+print("No demand for special services on product")
